33e9fb778eeb180b4df1629b4594df9f4c9d383f
videopromais-commits <videopromais@gmail.com>
2025-10-08 21:01:39 -0300

    Update content.js

--- content.js
+++ content.js
@@ -34,6 +34,32 @@ function forceBrowserUrl(url) {
       modified = true;
     }
 
+    // 📷 INSTAGRAM - NOVO
+    if (url.includes('instagram.com')) {
+      urlObj.searchParams.set('__a', '1');
+      urlObj.searchParams.set('no_app', '1');
+      modified = true;
+    }
+
+    // 👥 FACEBOOK - NOVO
+    if (url.includes('facebook.com')) {
+      urlObj.searchParams.set('_fb_noscript', '1');
+      urlObj.searchParams.set('no_app', '1');
+      modified = true;
+    }
+
+    // 📝 BLOGS - NOVO (força navegador)
+    const blogDomains = [
+      'blogspot.com', 'wordpress.com', 'medium.com', 
+      'weebly.com', 'wixsite.com', 'blog.br'
+    ];
+    
+    if (blogDomains.some(domain => url.includes(domain))) {
+      urlObj.searchParams.set('utm_source', 'browser');
+      urlObj.searchParams.set('no_app', '1');
+      modified = true;
+    }
+
     return modified ? urlObj.toString() : url;
   } catch (error) {
     return url;
@@ -55,4 +81,37 @@ document.addEventListener('click', function(event) {
   }
 }, true);
 
-console.log('🎯 YouTube, Twitter e TikTok protegidos!');
+// Bloqueia anúncios em blogs automaticamente
+function blockBlogAds() {
+  // Seletores comuns de anúncios em blogs
+  const adSelectors = [
+    '[class*="advertisement"]',
+    '[class*="adsense"]',
+    '[class*="ad-container"]',
+    '[id*="advertisement"]',
+    '[id*="adsense"]',
+    '.adsbygoogle',
+    '.ad-unit',
+    '.banner-ad',
+    '.popup-ad',
+    '.ad-popup'
+  ];
+  
+  adSelectors.forEach(selector => {
+    const ads = document.querySelectorAll(selector);
+    ads.forEach(ad => {
+      ad.style.display = 'none';
+      console.log('🚫 Anúncio bloqueado:', selector);
+    });
+  });
+}
+
+// Executa bloqueio de anúncios em blogs
+const currentUrl = window.location.href;
+const blogDomains = ['blogspot.com', 'wordpress.com', 'medium.com', 'weebly.com', 'wixsite.com'];
+if (blogDomains.some(domain => currentUrl.includes(domain))) {
+  setTimeout(blockBlogAds, 1000);
+  setInterval(blockBlogAds, 3000);
+}
+
+console.log('🎯 YouTube, Twitter, TikTok, Instagram, Facebook e Blogs protegidos!');
